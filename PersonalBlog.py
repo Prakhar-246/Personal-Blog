c47f7fa276d37d32bdbb87db2412fe969456c5ea
Prakhar Sharma <129146499+Prakhar-246@users.noreply.github.com>
2026-07-03 21:23:14 +0530

    Update PersonalBlog.py

--- PersonalBlog.py
+++ PersonalBlog.py
@@ -1,1 +1,24 @@
-import json
+import random
+
+# secret_number = random.randint(1, 100)
+# print(secret_number)
+
+
+
+
+
+
+
+
+
+
+
+
+print("Hye welcome to this number guessing game")
+name = input("Enter you name: ")
+print(f"Hye {name} choose a level to play the game")
+print(f"Press 1 for easy \nPress 2 for moderate\nPress 3 for hard")
+level = int(input("Enter a number: "))
+
+if level == 1:
+    pass
